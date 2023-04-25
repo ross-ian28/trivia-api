adb368e187dd0431b03bb32a1f26bc518a49aca6
Ian Ross <ianrossdrums@gmail.com>
2023-04-24 23:00:34 -0600

    Add html entities gem to get rid of HTML-encoded characters

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,11 +1,9 @@
-# take in category, dificulty, maybe questions
 require "httparty"
-require "pry"
+
 class Api::V1::QuestionsController < ApplicationController
   def create
     response = HTTParty.get("https://opentdb.com/api.php?amount=10&difficulty=#{params[:difficulty]}")
     data = JSON.parse(response.body, symbolize_names: true)[:results]
-    # render json: data
     render json: QuestionsSerializer.format_questions(data)
   end
 
--- app/serializers/questions_serializer.rb
+++ app/serializers/questions_serializer.rb
@@ -1,7 +1,21 @@
+require 'htmlentities'
+
 class QuestionsSerializer
   def self.format_questions(questions)
+    entities = HTMLEntities.new
+
+    sanitized_questions = questions.map do |question|
+      {
+        question: entities.decode(question[:question]),
+        category: question[:category],
+        difficulty: question[:difficulty],
+        correct_answer: entities.decode(question[:correct_answer]),
+        incorrect_answers: question[:incorrect_answers].map { |answer| entities.decode(answer) }
+      }
+    end
+
     {
-      questions: questions,
+      questions: sanitized_questions,
       score: 0
     }
   end
