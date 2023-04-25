76b55cf04625697824888bbb8df334b75d3e6081
Ian Ross <ianrossdrums@gmail.com>
2023-04-24 21:25:10 -0600

    Change reuest route

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -2,7 +2,7 @@
 require "httparty"
 require "pry"
 class Api::V1::QuestionsController < ApplicationController
-  def index
+  def create
     response = HTTParty.get("https://opentdb.com/api.php?amount=10&difficulty=#{params[:difficulty]}")
     data = JSON.parse(response.body, symbolize_names: true)[:results]
     render json: data
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "/questions", to: "questions#index"
+      post "/questions", to: "questions#create"
     end
   end
 end
