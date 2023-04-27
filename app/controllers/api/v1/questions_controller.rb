042eaaba5764742b2df5b63e76fcd892c7833176
Ian Ross <ianrossdrums@gmail.com>
2023-04-27 00:06:49 -0600

    DRY file and fix parsing error

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -3,14 +3,13 @@ require "pry"
 class Api::V1::QuestionsController < ApplicationController
   def create
     if params[:category] != ""
-      response = HTTParty.get("https://opentdb.com/api.php?amount=10&category=#{params[:category].to_i}&difficulty=#{params[:difficulty]}")
-      data = JSON.parse(response.body, symbolize_names: true)[:results]
-      render json: QuestionsSerializer.format_questions(data)
+        response = HTTParty.get("https://opentdb.com/api.php?amount=10&category=#{params[:category].to_i}&difficulty=#{params[:difficulty]}")
     else
       response = HTTParty.get("https://opentdb.com/api.php?amount=10&difficulty=#{params[:difficulty]}")
-      data = JSON.parse(response.body, symbolize_names: true)[:results]
-      render json: QuestionsSerializer.format_questions(data)
     end
+
+    data = JSON.parse(response.body, symbolize_names: true)[:results]
+    render json: QuestionsSerializer.format_questions(data), content_type: "application/json"
   end
 
   private
