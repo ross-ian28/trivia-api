477eed322c55a61f0491e587bc833242d792cf09
Ian Ross <ianrossdrums@gmail.com>
2023-04-26 18:10:29 -0600

    Add category parameter

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,15 +1,21 @@
 require "httparty"
-
+require "pry"
 class Api::V1::QuestionsController < ApplicationController
   def create
-    response = HTTParty.get("https://opentdb.com/api.php?amount=10&difficulty=#{params[:difficulty]}")
-    data = JSON.parse(response.body, symbolize_names: true)[:results]
-    render json: QuestionsSerializer.format_questions(data)
+    if params[:category] != ""
+      response = HTTParty.get("https://opentdb.com/api.php?amount=10&category=#{params[:category].to_i}&difficulty=#{params[:difficulty]}")
+      data = JSON.parse(response.body, symbolize_names: true)[:results]
+      render json: QuestionsSerializer.format_questions(data)
+    else
+      response = HTTParty.get("https://opentdb.com/api.php?amount=10&difficulty=#{params[:difficulty]}")
+      data = JSON.parse(response.body, symbolize_names: true)[:results]
+      render json: QuestionsSerializer.format_questions(data)
+    end
   end
 
   private
 
   def question_params
-    params.permit(:difficulty)
+    params.permit(:difficulty, :category)
   end
 end
