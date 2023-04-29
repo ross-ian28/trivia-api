78795f95cb0c4b9fdf57e8838ed0f8cb0e977b1d
Ian Ross <ianrossdrums@gmail.com>
2023-04-29 12:00:36 -0600

    Update cors headers and expose

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://trivia-ross-ian28.vercel.app', 'https://trivia-api-edqv.onrender.com'
 
     resource '/api/*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      headers: ['Content-Type': 'application/json'],
+      methods: :post,
+      expose: ['Access-Control-Allow-Origin'],
+      credentials: true
   end
 end
