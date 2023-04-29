b24e9e77cba2d3993049dbd53c13c4ee6c6fe9ca
Ian Ross <ianrossdrums@gmail.com>
2023-04-28 23:15:36 -0600

    Add cors for mobile

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'https://trivia-g0noib7o8-ross-ian28.vercel.app'
     resource '/api/*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization', 'Uid', 'Client', 'Access-Token', 'expiry', 'token-type'],
+      max_age: 600
   end
 end
