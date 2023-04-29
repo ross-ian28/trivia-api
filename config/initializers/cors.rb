69a5b6ac128a3221221a44d50742ee2eaeb3cc2d
Ian Ross <ianrossdrums@gmail.com>
2023-04-29 09:59:19 -0600

    Update render files

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://trivia-4o44silbc-ross-ian28.vercel.app/"
 
     resource "*",
       headers: :any,
