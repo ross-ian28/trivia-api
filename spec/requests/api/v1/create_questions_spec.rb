3f3835529c763dc673488e18b309023dae88053e
Ian Ross <ianrossdrums@gmail.com>
2023-04-30 00:41:50 -0600

    Create questions request spec

---
+++ spec/requests/api/v1/create_questions_spec.rb
@@ -0,0 +1,69 @@
+require "rails_helper"
+require "pry"
+
+RSpec.describe "POST #create" do
+  describe "questions request" do
+    it "returns questions created with difficulty and category", :vcr do
+      params = {
+        difficulty: "easy",
+        category: "9",
+      }
+      headers = { "Content-Type" => "application/json" }
+
+      post "/api/v1/questions", headers: headers, params: JSON.generate(params)
+
+      questions = JSON.parse(response.body, symbolize_names: true)[:questions]
+
+      expect(response).to be_successful
+
+      expect(questions).to be_an(Array)
+
+      expect(questions.length).to eq(10)
+      expect(questions[0]).to include :question, :category, :difficulty, :correct_answer, :incorrect_answers
+      expect(questions[0][:question]).to be_a(String)
+      expect(questions[0][:category]).to be_a(String)
+      expect(questions[0][:difficulty]).to be_a(String)
+      expect(questions[0][:correct_answer]).to be_a(String)
+      expect(questions[0][:incorrect_answers]).to be_an(Array)
+    end
+
+    it "returns questions created with difficulty and no category", :vcr do
+      params = {
+        difficulty: "easy",
+        category: "",
+      }
+      headers = { "Content-Type" => "application/json" }
+
+      post "/api/v1/questions", headers: headers, params: JSON.generate(params)
+
+      questions = JSON.parse(response.body, symbolize_names: true)[:questions]
+
+      expect(response).to be_successful
+
+      expect(questions).to be_an(Array)
+      expect(questions.length).to eq(10)
+      expect(questions[0]).to include :question, :category, :difficulty, :correct_answer, :incorrect_answers
+      expect(questions[0][:question]).to be_a(String)
+      expect(questions[0][:category]).to be_a(String)
+      expect(questions[0][:difficulty]).to be_a(String)
+      expect(questions[0][:correct_answer]).to be_a(String)
+      expect(questions[0][:incorrect_answers]).to be_an(Array)
+    end
+    # it "checks question params", :vcr do
+    #   params = {
+    #     difficulty: "easy",
+    #     category: ""
+    #   }
+    #   headers = { "Content-Type" => "application/json" }
+    #
+    #   post "/api/v1/questions", headers: headers, params: JSON.generate(params.merge(invalid_param: "invalid"))
+    #
+    #   questions = JSON.parse(response.body, symbolize_names: true)[:questions]
+    #
+    #   expect(response).to be_successful
+    #
+    #   expect(response).to have_http_status(:unprocessable_entity)
+    #   expect(response.body).to match(/Unpermitted parameters: invalid_param/)
+    # end
+  end
+end
